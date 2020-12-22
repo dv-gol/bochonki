31a8dbf87ceb550e9805d8e8913d812bcad34c2a
Dasha <71096430+dv-gol@users.noreply.github.com>
2020-12-23 00:06:23 +0500

    добавлен код

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <iostream>
+#include <ctime>
+#include <random>
+#include <vector>
+using namespace std;
+int main()
+{
+	int n;
+	vector <int> v;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите количество бочек: ";
+	cin >> n;
+	int i = 0;
+	int number = 0;
+	string dummy;
+	while (i < n) {
+		cout << "Нажмите любую клавишу, чтобы вытянуть бочонок: ";
+		cin >> dummy;
+		do {
+			number = rand() % n;
+		} while (find(v.begin(), v.end(), number) != v.end());
+		v.push_back(number);
+		cout << "Число на бочонке: " << number + 1 << endl;
+		i++;
+	}
+	cout << "Все бочонки были вытащены";
+}
